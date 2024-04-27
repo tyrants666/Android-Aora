a8051d06ce0ad0abf850df8dcf7c9b4a308a95df
tyrants666 <akashyadav.iiita@gmail.com>
2024-04-27 09:55:29 +0545

    feature/tailwind + fonts + assets added

--- app/index.jsx
+++ app/index.jsx
@@ -4,19 +4,20 @@ import { Link } from 'expo-router';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Aora</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Aora</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+// ================= Vanillla Styles  ==================
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
